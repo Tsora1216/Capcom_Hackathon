0ceb2b80e098e67495efc0b310866e4333dbfb72
Tsora1216 <so12ra16@icloud.com>
2022-11-12 13:43:39 +0900

    main

---
+++ capcom.py
@@ -0,0 +1,11 @@
+#py_test2.py
+
+import random
+def func(data):
+    print("hogehoge")
+    a = data
+    print(a)
+    list_data = [14,2,3.5,28]
+    list_data.append(a)
+    b = random.choice(list_data)
+    return b
